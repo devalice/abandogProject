2a4ec5726b543aeb2b44c7d643692c32a6586f1d
nohyeonju <nohyeonju@localhost>
2022-01-21 16:04:06 +0900

    abandogAdoptput abandogAdoptputReply

---
+++ abandoneddog/src/main/java/kr/co/abandog/entity/AbandogAdoptPutReply.java
@@ -0,0 +1,40 @@
+package kr.co.abandog.entity;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name="abandog_adopt_put_reply")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AbandogAdoptPutReply extends BaseEntity{
+	
+		
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name="animal_adopt_put_reply", nullable=false)
+private int put_reply_num;
+
+@Column(name="put_reply_content", length=200, nullable=true)
+private Date del_dtm;
+
+@Column(name="put_num",nullable=false)
+private int put_num;
+
+@Column(name="reply_writer", length=20, nullable=false)
+private String reply_writer;
+	
+}
